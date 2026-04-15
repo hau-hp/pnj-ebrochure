1e6ef738eeada0c9aca56fc73c68a82bbcedc4ba
hau-hp <hau.hp@pnj.com.vn>
2026-04-15 15:03:24 +0700

    Add pnj-ebrochure.site to adminAllowedOrigins

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ window.__PNJ_APP_CONFIG__ = {
         'http://localhost:5500',
         'http://127.0.0.1:3000',
         'http://localhost:3000',
-        'https://hau-hp.github.io'
+        'https://hau-hp.github.io',
+                'https://pnj-ebrochure.site'
     ],
     cloudinaryCloudName: 'dhe7uziws',
     cloudinaryUploadPreset: 'pnj_unsigned'
